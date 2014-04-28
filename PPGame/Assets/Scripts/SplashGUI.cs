da70114422f7353e0fbb8451ec7214c6578d506b
MrMekko <t_popowitz@hotmail.com>
2014-04-28 10:32:12 +0200

    Splash screen
    Splash screen added. Will play before the main scene. Main scene will
    start when space is pressed. The .psd file needs to be redone as it is
    the texture for the splash screen.

---
+++ PPGame/Assets/Scripts/SplashGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashGUI : MonoBehaviour {
+
+	public GUISkin customSkin = null;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+				Application.LoadLevel("Scene1");
+			}
+	}
+		void OnGUI(){
+			GUI.Box (new Rect (1, 1, 1, 1), " ");
+		}
+	}
+		
+	
+	
+
